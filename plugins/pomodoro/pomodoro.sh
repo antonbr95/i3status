463eae85babc4c63557b2ad84cbf9178cd94adb5
antonbr95 <wanton.amb@gmail.com>
2018-05-16 13:37:51 +0200

    added colors and i3status.conf

--- plugins/pomodoro/pomodoro.sh
+++ plugins/pomodoro/pomodoro.sh
@@ -1,9 +1,12 @@
 #! 
 
 
+echo 1 > mode
 ./run_pomodoro $@
 while [ true ]; do
+	echo 2 > mode
 	./run_pomodoro 5 0
+	echo 1 > mode
 	./run_pomodoro 25
 done
 
--- src/print_pomodoro.c
+++ src/print_pomodoro.c
@@ -34,13 +34,31 @@ static char *apply_pomodoro_format(const char *fmt, char *outwalk, int pomo_minu
 }
 
 void print_pomodoro(yajl_gen json_gen, char *buffer, const char *format, const char *format_pomo, const char *format_break) {
-    char *outwalk = buffer;
 
-    const char *selected_format = format_pomo;
-    bool colorful_output = false;
+    bool colorful_output = true;
+    char *outwalk = buffer;
 
     // TODO: put your path to pomodoro files here
     char *path = "/home/anton/programming/i3status/plugins/pomodoro/";
+
+	// read mode
+    char mode_filename[256];
+    sprintf(mode_filename, "%smode", path);
+	int mode = read_number_from_file(mode_filename);
+
+    const char *selected_format = format_pomo;
+
+    if (mode == 0) {
+        selected_format = format;
+    }
+    else if (mode == 1) {
+        selected_format = format_pomo;
+        START_COLOR("color_bad");
+    }
+    else if (mode == 2) {
+        selected_format = format_break;
+        START_COLOR("color_good");
+    }
 
 	// read minutes
     char minutes_filename[256];
